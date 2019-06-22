b86f9807304baf355c4379452a43a28025228b6b
Mosen <mosen@users.noreply.github.com>
2019-06-22 22:52:36 +1000

    Initial commit of generated connector structure

---
+++ src/main/java/com/github/mosen/AzureStorageSourceConnector.java
@@ -0,0 +1,68 @@
+package com.github.mosen;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.connect.connector.Task;
+import org.apache.kafka.connect.errors.ConnectException;
+import org.apache.kafka.connect.source.SourceConnector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.github.jcustenborder.kafka.connect.utils.VersionUtil;
+import com.github.jcustenborder.kafka.connect.utils.config.Description;
+import com.github.jcustenborder.kafka.connect.utils.config.DocumentationImportant;
+import com.github.jcustenborder.kafka.connect.utils.config.DocumentationNote;
+import com.github.jcustenborder.kafka.connect.utils.config.DocumentationTip;
+import com.github.jcustenborder.kafka.connect.utils.config.TaskConfigs;
+import com.github.jcustenborder.kafka.connect.utils.config.Title;
+
+@Description("The Azure Storage source connector retrieves Azure Table storage rows as Kafka messages")
+@DocumentationImportant("This is a important information that will show up in the documentation.")
+@DocumentationTip("This is a tip that will show up in the documentation.")
+@Title("Azure Storage Connector") //This is the display name that will show up in the documentation.
+@DocumentationNote("This is a note that will show up in the documentation")
+public class AzureStorageSourceConnector extends SourceConnector {
+  /*
+    Your connector should never use System.out for logging. All of your classes should use slf4j
+    for logging
+ */
+  private static Logger log = LoggerFactory.getLogger(AzureStorageSourceConnector.class);
+  private AzureStorageSourceConnectorConfig config;
+
+  @Override
+  public String version() {
+    return VersionUtil.version(this.getClass());
+  }
+
+  @Override
+  public void start(Map<String, String> map) {
+    config = new AzureStorageSourceConnectorConfig(map);
+
+    //TODO: Add things you need to do to setup your connector.
+  }
+
+  @Override
+  public Class<? extends Task> taskClass() {
+    //TODO: Return your task implementation.
+    return AzureStorageTableSourceTask.class;
+  }
+
+  @Override
+  public List<Map<String, String>> taskConfigs(int i) {
+    //TODO: Define the individual task configurations that will be executed.
+
+    throw new UnsupportedOperationException("This has not been implemented.");
+  }
+
+  @Override
+  public void stop() {
+    //TODO: Do things that are necessary to stop your connector.
+  }
+
+  @Override
+  public ConfigDef config() {
+    return AzureStorageSourceConnectorConfig.config();
+  }
+}
---
+++ src/main/java/com/github/mosen/AzureStorageSourceConnectorConfig.java
@@ -0,0 +1,43 @@
+package com.github.mosen;
+
+import org.apache.kafka.common.config.AbstractConfig;
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.common.config.ConfigDef.Type;
+import org.apache.kafka.common.config.ConfigDef.Importance;
+import com.github.jcustenborder.kafka.connect.utils.config.ConfigKeyBuilder;
+import java.util.Map;
+
+
+
+public class AzureStorageSourceConnectorConfig extends AbstractConfig {
+
+  public static final String AZURE_CONNECTION_STRING_CONFIG = "azure.connectionstring";
+  private static final String AZURE_CONNECTION_STRING_DOC = "The Azure storage account connection string as shown in " +
+          "the portal";
+
+  public static final String AZURE_STORAGE_TYPE_CONFIG = "azure.storagetype";
+  private static final String AZURE_STORAGE_TYPE_DOC = "The type of Azure storage being read. Only `table` is " +
+          "supported right now.";
+
+  public static final String AZURE_TABLE_NAME_CONFIG = "azure.table.name";
+  private static final String AZURE_TABLE_NAME_DOC = "The name of the table to use as a source";
+
+  public final String azureConnectionString;
+
+  public AzureStorageSourceConnectorConfig(Map<?, ?> originals) {
+    super(config(), originals);
+    this.azureConnectionString = this.getString(AZURE_CONNECTION_STRING_CONFIG);
+  }
+
+  public static ConfigDef config() {
+    return new ConfigDef()
+        .define(
+            ConfigKeyBuilder.of(AZURE_CONNECTION_STRING_CONFIG, Type.STRING)
+                .documentation(AZURE_CONNECTION_STRING_DOC)
+                .importance(Importance.HIGH)
+                .build()
+        ).define(
+
+          );
+  }
+}
